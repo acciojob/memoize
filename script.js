400ba5c092eb51d1eb746b7acf490c57b840e313
HarshKhandelwal101 <78348500+HarshKhandelwal101@users.noreply.github.com>
2023-03-10 10:04:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+const defaultResolver = (...args) => JSON.stringify(args)
+function memoize(callback, resolver=defaultResolver) { 
+ 
+}
+// Do not edit the line below.
+
+module.exports = memoize;
